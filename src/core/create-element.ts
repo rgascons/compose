be92208df92b04f7d6974faa63d30329a63dd5b9
Ricard Gascons <gascons1995@gmail.com>
2022-08-29 16:46:43 -0700

    add converstion from js styles to css

--- src/core/create-element.ts
+++ src/core/create-element.ts
@@ -1,4 +1,17 @@
+const convertJStoCSS = (jsStyles) => {
+  let cssString = "";
+  for (let objectKey in jsStyles) {
+    cssString += objectKey.replace(/([A-Z])/g, (g) => `-${g[0].toLowerCase()}`) + ": " + jsStyles[objectKey] + ";\n";
+  }
+
+  return cssString;
+};
+
 export const createElement = (tagName, attrs = {}, ...children) => {
+  if (attrs?.["style"]) {
+    attrs["style"] = convertJStoCSS(attrs["style"]);;
+  }
+
   const elem = Object.assign(document.createElement(tagName), attrs);
   for (const child of children) {
     if (Array.isArray(child)) elem.append(...child);
